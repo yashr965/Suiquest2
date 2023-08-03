83e604eb2ee1c5be45ed896c11bbf2025fe7c9ca
Aanya Singh <95902545+0xAanya@users.noreply.github.com>
2023-08-03 23:32:49 +0530

    Update runbot.js

--- runbot.js
+++ runbot.js
@@ -2,6 +2,10 @@ const { spawn } = require('child_process');
 
 let successfulRuns = 0;
 
+function getRandomTimeout(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function runBot() {
   const child = spawn('npm', ['run', 'test:unit', 'bot/index.spec.ts'], {
     stdio: 'inherit',
@@ -16,14 +20,15 @@ function runBot() {
     if (code === 0) {
       successfulRuns++;
       console.log(
-        `👍🏻 Tests completed successfully. Bot has run successfully ${successfulRuns} time(s). Re-running in 5 seconds...`
+        `👍🏻Tests completed successfully. Bot has run successfully ${successfulRuns} time(s). Re-running in 5 seconds...`
       );
     } else {
       console.log(
         `❌ Tests failed. Bot has run successfully ${successfulRuns} time(s). Re-running in 5 seconds...`
       );
     }
-    setTimeout(runBot, 5000); // Restart the bot after a 5-second delay, regardless of success or failure
+    const randomTimeout = getRandomTimeout(5000, 10000);
+    setTimeout(runBot, randomTimeout); // Restart the bot after a random timeout between 5000ms and 10000ms, regardless of success or failure
   });
 }
 
